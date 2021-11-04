dee535abc0efd4e77e8d96432c96a422ce1f9f51
Charlie Prince <charlie.prince@reasondigital.com>
2021-11-04 22:24:57 +0000

    Add api keys:

--- weatherapp/src/store/index.js
+++ weatherapp/src/store/index.js
@@ -1,8 +1,23 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    api_key: "1698ad14266892027f713998c5613169",
+    url_base: "https://api.openweathermap.org/data/2.5/",
+    places_api_key: "AIzaSyDejLbYOEsYMKshI1r8vqOREa9Ly_dEwKk",
+  },
   mutations: {},
   actions: {},
   modules: {},
+  getters: {
+    getApiKey: state => {
+      return state.api_key;
+    },
+    urlBase: state => {
+      return state.url_base;
+    },
+    placesApiKey: state => {
+      return state.places_api_key;
+    }
+  }
 });

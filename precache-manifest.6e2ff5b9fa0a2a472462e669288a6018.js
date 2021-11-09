5b63b2b264767ca0c878eed0cd8f0a3c47019ceb
Charlie Prince <charlie.prince@reasondigital.com>
2021-11-09 17:32:10 +0000

    build for prod

---
+++ precache-manifest.6e2ff5b9fa0a2a472462e669288a6018.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ebe5069bcd35d3812e",
+    "url": "/css/about.4fbea550.css"
+  },
+  {
+    "revision": "766f9ae679b8840e229a",
+    "url": "/css/app.e0d99b0e.css"
+  },
+  {
+    "revision": "831722db3d17e323a320d65db7c0d7c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0ebe5069bcd35d3812e",
+    "url": "/js/about.a5c876b2.js"
+  },
+  {
+    "revision": "766f9ae679b8840e229a",
+    "url": "/js/app.4b9bc6c5.js"
+  },
+  {
+    "revision": "6568eef0bb2bcb53eda9",
+    "url": "/js/chunk-vendors.c3901337.js"
+  },
+  {
+    "revision": "870aaa2b14790f19834149f429f63343",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
